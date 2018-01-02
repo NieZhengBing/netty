a4c173b9e5d2a11e13deabb2cebebbc5713046df
NieZhengBing <nzb15555196162@gmail.com>
2018-01-02 18:23:59 +0800

    add listen in main

--- src/com/nzb/netty/nio/NIOServer.java
+++ src/com/nzb/netty/nio/NIOServer.java
@@ -23,6 +23,7 @@ public class NIOServer {
 	public static void main(String[] args) throws IOException {
 		NIOServer server = new NIOServer();
 		server.initServer(8000);
+		server.listen();
 	}
 
 	/**
